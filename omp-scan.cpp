c3c79666b6d0c077d53f4e089494a3c677402439
Yifei <yz6994@nyu.edu>
2023-04-02 11:47:34 -0400

    finish q2 code

--- omp-scan.cpp
+++ omp-scan.cpp
@@ -1,25 +1,49 @@
 #include <algorithm>
 #include <stdio.h>
+#include <iostream>
 #include <math.h>
 #include <omp.h>
+#include <cassert>  
+
 
 // Scan A array and write result into prefix_sum array;
 // use long data type to avoid overflow
 void scan_seq(long* prefix_sum, const long* A, long n) {
-  if (n == 0) return;
-  prefix_sum[0] = 0;
-  for (long i = 1; i < n; i++) {
-    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
-  }
+    if (n == 0) return;
+    prefix_sum[0] = A[0];
+    //#pragma omp parallel for
+    for (long i = 1; i < n; i++) {
+        prefix_sum[i] = prefix_sum[i-1] + A[i];
+    }
 }
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
-  int p = omp_get_num_threads();
-  int t = omp_get_thread_num();
-  // Fill out parallel scan: One way to do this is array into p chunks
-  // Do a scan in parallel on each chunk, then share/compute the offset
-  // through a shared vector and update each chunk by adding the offset
-  // in parallel
+    //int p = omp_get_num_threads();
+    int p = 8;
+    //int t = omp_get_thread_num();
+    // Fill out parallel scan: One way to do this is array into p chunks
+    // Do a scan in parallel on each chunk, then share/compute the offset
+    // through a shared vector and update each chunk by adding the offset
+    // in parallel
+    int chunksize = ceil(n / double(p));
+
+    #pragma omp parallel for num_threads(p)
+    for (int i = 0; i < p; i++){
+        long head = i * chunksize;
+        prefix_sum[head] = A[head];
+        for (int j = head + 1; j < std::min(head + chunksize, n); j++){
+            prefix_sum[j] = prefix_sum[j - 1] + A[j];
+        }
+    }
+
+    for (int i = 1; i < p; i++){
+        long head = i * chunksize;
+        long correction = prefix_sum[head - 1];
+        #pragma omp parallel for num_threads(p)
+        for (int j = head; j < std::min(head + chunksize, n); j++){
+            prefix_sum[j] += correction;
+        }
+    }
 }
 
 int main() {
@@ -39,7 +63,17 @@ int main() {
   printf("parallel-scan   = %fs\n", omp_get_wtime() - tt);
 
   long err = 0;
-  for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+  for (long i = 0; i < N; i++) {
+      err = std::max(err, std::abs(B0[i] - B1[i]));
+      // if (err != 0) {
+      //   std::cout << "B0[" << i << "] = " << B0[i] << std::endl;
+      //   std::cout << "B1[" << i << "] = " << B1[i] << std::endl;
+      //   std::cout << "B0[" << i <<"] - B1[" << i << "] = " << B0[i] - B1[i] << std::endl;
+      //   std::cout << "i = " << i << ", err = " << err << "\n";
+      //   break;
+      // }
+      //assert (err == 0);
+  }
   printf("error = %ld\n", err);
 
   free(A);
